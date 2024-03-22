d8dab0127ecfbe4cb5bc0da361ac13e0156a777a
danielmarin95 <94467685+danielmarin95@users.noreply.github.com>
2024-03-21 22:55:29 -0600

    added catalogue functionalities

--- js/catalogo.js
+++ js/catalogo.js
@@ -105,12 +105,22 @@ function createProductItem(product, pageClass){
             </section>
             <section class="price-and-button">
                 <p>₡${parseFloat(product.precio).toFixed(2)}</p>
-                <button>Al carrito</button>
+                <span class="add-to-cart-catalogue"
+                    data-productid="${product.id}"
+                    data-productname="${product.nombre}"
+                    data-productdescription="${product.descripcion}"
+                    data-productprice="${product.precio}"
+                    data-productimage="${product.imagen}"
+                    data-productquantity="${product.cantidad}"
+                    data-productunit="${product.unidad}"
+                    onclick=showModal(this)><i class="fa-solid fa-cart-plus fa-lg"></i></span>
             </section>
         </section>
     `;
     return article;
 }
+
+
 
 /**
  * Returns a string with the stars for the calification number
@@ -123,6 +133,51 @@ function getStarsCalification(calification){
         stars += "⭐";
     }
     return stars;
+}
+
+/**
+ * Shows the modal to add the product to the cart
+ */
+
+function showModal(target) {
+    document.getElementById('modal-product-name').innerHTML = target.getAttribute('data-productname');
+    document.getElementById('modal-product-description').innerHTML = target.getAttribute('data-productdescription');
+    document.getElementById('modal-product-price').innerHTML = "₡" + parseFloat(target.getAttribute('data-productprice')).toFixed(2);
+    document.getElementById('modal-product-image').src = target.getAttribute('data-productimage');
+    document.getElementById('modal-product-image').alt = target.getAttribute('data-productname');
+    document.getElementById('modal-product-quantity').max = target.getAttribute('data-productquantity');
+    document.getElementById('modal-product-unit').innerHTML = target.getAttribute('data-productunit');
+    let modal = document.getElementById('modal');
+    modal.style.display = 'block';
+}
+
+/**
+ * Closes the modal to add to the cart
+ */
+function closeModal(event){
+    if (event.target.id === "modal") {
+        let modal = document.getElementById('modal');
+        // If it was, hide the modal
+        modal.style.display = 'none';
+    }
+}
+
+
+function addToCart(target){
+    target.innerHTML = `<i class="fa-solid fa-square-check fa-lg" style="color: #005b4d;"></i>`;
+    setTimeout(() => {
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        if(!cart){
+            cart = [];
+        }
+        let product = {
+            id: target.getAttribute('data-productid'),
+        }
+        cart.push(product);
+        localStorage.setItem('cart', JSON.stringify(cart));
+        closeModal({target: {id: "modal"}});
+        target.innerHTML = `<i class="fa-solid fa-cart-plus fa-lg"></i>`;
+    }, 500);
 }
 
 
@@ -141,6 +196,8 @@ const products = [
         tramo: "Piña de Tía Panchita",
         calificacion: 4,
         precio: 400,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://www.saluzzocr.com/wp-content/uploads/2021/08/pinia-el-sabor-de-la-frecura-1.jpg"
     },
     {
@@ -150,6 +207,8 @@ const products = [
         tramo: "Naranjas de Tía Panchita",
         calificacion: 5,
         precio: 450,
+        cantidad: 80,
+        unidad: "unidades",
         imagen: "https://imagenes.20minutos.es/files/image_640_auto/uploads/imagenes/2023/06/12/2-naranjas.jpeg"
     },
     {
@@ -159,6 +218,8 @@ const products = [
         tramo: "Mango de Tía Panchita",
         calificacion: 3,
         precio: 500,
+        cantidad: 8,
+        unidad: "gramos",
         imagen: "https://www.finedininglovers.com/es/sites/g/files/xknfdk1706/files/styles/article_1200_800_fallback/public/2021-10/mango%C2%A9iStock.jpg?itok=b0BXEvPw"
     },
     {
@@ -168,6 +229,8 @@ const products = [
         tramo: "Papaya de Tía Panchita",
         calificacion: 5,
         precio: 550,
+        cantidad: 82,
+        unidad: "unidades",
         imagen: "https://eldiariony.com/wp-content/uploads/sites/2/2023/07/shutterstock_1528885103.jpg?w=1200"
     },
     {
@@ -177,6 +240,8 @@ const products = [
         tramo: "Melón de Tía Panchita",
         calificacion: 2,
         precio: 600,
+        cantidad: 65,
+        unidad: "kg",
         imagen: "https://elpoderdelconsumidor.org/wp-content/uploads/2023/02/melon.jpg"
     },
     {
@@ -186,6 +251,8 @@ const products = [
         tramo: "Ciruela de Tía Panchita",
         calificacion: 5,
         precio: 650,
+        cantidad: 40,
+        unidad: "kg",
         imagen: "https://www.infocampo.com.ar/wp-content/uploads/2021/02/ciruelas-senasa-infocampo.jpg"
     },
     {
@@ -195,6 +262,8 @@ const products = [
         tramo: "Papaya de Tía Panchita",
         calificacion: 5,
         precio: 550,
+        cantidad: 90,
+        unidad: "unidades",
         imagen: "https://eldiariony.com/wp-content/uploads/sites/2/2023/07/shutterstock_1528885103.jpg?w=1200"
     },
     {
@@ -204,6 +273,8 @@ const products = [
         tramo: "Naranjas de Tía Panchita",
         calificacion: 5,
         precio: 450,
+        cantidad: 450,
+        unidad: "kg",
         imagen: "https://imagenes.20minutos.es/files/image_640_auto/uploads/imagenes/2023/06/12/2-naranjas.jpeg"
     },
     {
@@ -213,6 +284,8 @@ const products = [
         tramo: "Mango de Tía Panchita",
         calificacion: 3,
         precio: 500,
+        cantidad: 20,
+        unidad: "unidades",
         imagen: "https://www.finedininglovers.com/es/sites/g/files/xknfdk1706/files/styles/article_1200_800_fallback/public/2021-10/mango%C2%A9iStock.jpg?itok=b0BXEvPw"
     },
     {
@@ -222,6 +295,8 @@ const products = [
         tramo: "Ciruela de Tía Panchita",
         calificacion: 5,
         precio: 650,
+        cantidad: 5,
+        unidad: "litros",
         imagen: "https://www.infocampo.com.ar/wp-content/uploads/2021/02/ciruelas-senasa-infocampo.jpg"
     },
     {
@@ -231,6 +306,8 @@ const products = [
         tramo: "Mango de Tía Panchita",
         calificacion: 3,
         precio: 500,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://www.finedininglovers.com/es/sites/g/files/xknfdk1706/files/styles/article_1200_800_fallback/public/2021-10/mango%C2%A9iStock.jpg?itok=b0BXEvPw"
     },
     {
@@ -240,6 +317,8 @@ const products = [
         tramo: "Papaya de Tía Panchita",
         calificacion: 5,
         precio: 550,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://eldiariony.com/wp-content/uploads/sites/2/2023/07/shutterstock_1528885103.jpg?w=1200"
     },
     {
@@ -249,6 +328,8 @@ const products = [
         tramo: "Melón de Tía Panchita",
         calificacion: 2,
         precio: 600,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://elpoderdelconsumidor.org/wp-content/uploads/2023/02/melon.jpg"
     },
     {
@@ -258,7 +339,20 @@ const products = [
         tramo: "Sandía de Tía Panchita",
         calificacion: 5,
         precio: 300,
+        cantidad: 95,
+        unidad: "unidades",
         imagen: "https://s1.eestatic.com/2021/08/18/ciencia/nutricion/605201362_200816272_1706x960.jpg"
+    },
+    {
+        id: 34,
+        nombre: "Ciruela",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        tramo: "Ciruela de Tía Panchita",
+        calificacion: 5,
+        precio: 650,
+        cantidad: 125,
+        unidad: "kg",
+        imagen: "https://www.infocampo.com.ar/wp-content/uploads/2021/02/ciruelas-senasa-infocampo.jpg"
     },
     {
         id: 21,
@@ -267,6 +361,8 @@ const products = [
         tramo: "Banano de Tía Panchita",
         calificacion: 1,
         precio: 350,
+        cantidad: 114,
+        unidad: "kg",
         imagen: "https://cnnespanol.cnn.com/wp-content/uploads/2014/01/120604032828-fresh-ripe-bananas-story-top-e1694020100421.jpg?quality=100&strip=info"
     },
     {
@@ -276,6 +372,8 @@ const products = [
         tramo: "Papaya de Tía Panchita",
         calificacion: 5,
         precio: 550,
+        cantidad: 184,
+        unidad: "kg",
         imagen: "https://eldiariony.com/wp-content/uploads/sites/2/2023/07/shutterstock_1528885103.jpg?w=1200"
     },
     {
@@ -285,6 +383,8 @@ const products = [
         tramo: "Melón de Tía Panchita",
         calificacion: 2,
         precio: 600,
+        cantidad: 142,
+        unidad: "kg",
         imagen: "https://elpoderdelconsumidor.org/wp-content/uploads/2023/02/melon.jpg"
     },
     {
@@ -294,6 +394,8 @@ const products = [
         tramo: "Ciruela de Tía Panchita",
         calificacion: 5,
         precio: 650,
+        cantidad: 174,
+        unidad: "kg",
         imagen: "https://www.infocampo.com.ar/wp-content/uploads/2021/02/ciruelas-senasa-infocampo.jpg"
     },
     {
@@ -303,6 +405,8 @@ const products = [
         tramo: "Papaya de Tía Panchita",
         calificacion: 5,
         precio: 550,
+        cantidad: 14,
+        unidad: "kg",
         imagen: "https://eldiariony.com/wp-content/uploads/sites/2/2023/07/shutterstock_1528885103.jpg?w=1200"
     },
     {
@@ -312,6 +416,8 @@ const products = [
         tramo: "Naranjas de Tía Panchita",
         calificacion: 5,
         precio: 450,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://imagenes.20minutos.es/files/image_640_auto/uploads/imagenes/2023/06/12/2-naranjas.jpeg"
     },
     {
@@ -321,6 +427,8 @@ const products = [
         tramo: "Mango de Tía Panchita",
         calificacion: 3,
         precio: 500,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://www.finedininglovers.com/es/sites/g/files/xknfdk1706/files/styles/article_1200_800_fallback/public/2021-10/mango%C2%A9iStock.jpg?itok=b0BXEvPw"
     },
     {
@@ -330,6 +438,8 @@ const products = [
         tramo: "Ciruela de Tía Panchita",
         calificacion: 5,
         precio: 650,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://www.infocampo.com.ar/wp-content/uploads/2021/02/ciruelas-senasa-infocampo.jpg"
     },
     {
@@ -339,6 +449,8 @@ const products = [
         tramo: "Mango de Tía Panchita",
         calificacion: 3,
         precio: 500,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://www.finedininglovers.com/es/sites/g/files/xknfdk1706/files/styles/article_1200_800_fallback/public/2021-10/mango%C2%A9iStock.jpg?itok=b0BXEvPw"
     },
     {
@@ -348,6 +460,8 @@ const products = [
         tramo: "Papaya de Tía Panchita",
         calificacion: 5,
         precio: 550,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://eldiariony.com/wp-content/uploads/sites/2/2023/07/shutterstock_1528885103.jpg?w=1200"
     },
     {
@@ -357,6 +471,8 @@ const products = [
         tramo: "Melón de Tía Panchita",
         calificacion: 2,
         precio: 600,
+        cantidad: 90,
+        unidad: "litros",
         imagen: "https://elpoderdelconsumidor.org/wp-content/uploads/2023/02/melon.jpg"
     },
     {
@@ -366,6 +482,8 @@ const products = [
         tramo: "Sandía de Tía Panchita",
         calificacion: 5,
         precio: 300,
+        cantidad: 200,
+        unidad: "unidades",
         imagen: "https://s1.eestatic.com/2021/08/18/ciencia/nutricion/605201362_200816272_1706x960.jpg"
     },
     {
@@ -375,6 +493,8 @@ const products = [
         tramo: "Banano de Tía Panchita",
         calificacion: 1,
         precio: 350,
+        cantidad: 1,
+        unidad: "kg",
         imagen: "https://cnnespanol.cnn.com/wp-content/uploads/2014/01/120604032828-fresh-ripe-bananas-story-top-e1694020100421.jpg?quality=100&strip=info"
     },
 ];
