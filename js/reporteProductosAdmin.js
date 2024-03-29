486972a1c410b5d4903f108670c1a89fb4f9cf95
mary <mgomezr@ucenfotec.ac.cr>
2024-03-28 21:15:19 -0600

    ajuste vistas mary

---
+++ js/reporteProductosAdmin.js
@@ -0,0 +1,67 @@
+const ProductsReport = [
+{
+    Categoria : "verduras",
+    Tramo : "La Rosas",
+    Producto : "Zanahoria",
+    Detalle : "zanahorias frescas, sembradas naturalmente y libre de quimicos",
+    Cantidad: "60",
+    PrecioSinImpuesto: "600",
+    PrecioConImpuesto: "700",
+
+},
+{
+    Categoria : "verduras",
+    Tramo : "Campo verde",
+    Producto : "Zanahoria",
+    Detalle : "zanahorias frescas, sembradas naturalmente y libre de quimicos",
+    Cantidad: "90",
+    PrecioSinImpuesto: "650",
+    PrecioConImpuesto: "750",
+},
+{
+    Categoria : "Fruta",
+    Tramo : "La Rosas",
+    Producto : "Manzana mixta",
+    Detalle : "Manzanas mixtas importadas desde california EEUU",
+    Cantidad: "30",
+    PrecioSinImpuesto: "1000",
+    PrecioConImpuesto: "1100",
+},
+{
+    Categoria : "Fruta",
+    Tramo : "Campo Verde",
+    Producto : "Manzana mixta",
+    Detalle : "frescas manzanas libres de quimicos",
+    Cantidad: "60",
+    PrecioSinImpuesto: "900",
+    PrecioConImpuesto: "1000",
+},
+{
+    Categoria : "verduras",
+    Tramo : "Granja saludable",
+    Producto : "cebolla",
+    Detalle : "Sembrada en San Carlos, libre de pesticidas y  quimicos",
+    Cantidad: "70",
+    PrecioSinImpuesto: "800",
+    PrecioConImpuesto: "825",
+},
+];
+buildTable(ProductsReport);
+
+function buildTable(data) {
+    const table = document.getElementById("tablereport");
+
+    for (let i = 0; i < data.length; i++) {
+        const row = `<tr>
+                        <td>${data[i].Categoria}</td>
+                        <td>${data[i].Tramo}</td>
+                        <td>${data[i].Producto}</td>
+                        <td>${data[i].Detalle}</td>
+                        <td>${data[i].Cantidad}</td>
+                        <td>${data[i].PrecioSinImpuesto}</td>
+                        <td>${data[i].PrecioConImpuesto}</td>
+                    </tr>`;
+        table.innerHTML += row;
+    }
+}
+    
